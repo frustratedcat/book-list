7f0013ffdcc4ed37fe23c2d5e02831b4989e98e6
frustratedcat <jaredbrown@skiff.com>
2023-07-26 16:52:50 -0400

    Remove unneeded html, add pointer to radio buttons, add javascript to work for switching search selection

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 "use strict";
+
+const searchBox = document.querySelector(".search-section-input");
+
+const radioSearchAuthor = document.getElementById("search-author");
+
+const radioSearchTitle = document.getElementById("search-title");
+
+radioSearchAuthor.addEventListener("click", function () {
+  radioSearchAuthor.checked = true;
+  radioSearchTitle.checked = false;
+  searchBox.placeholder = "Search Author";
+});
+
+radioSearchTitle.addEventListener("click", function () {
+  radioSearchAuthor.checked = false;
+  radioSearchTitle.checked = true;
+  searchBox.placeholder = "Search Title";
+});
